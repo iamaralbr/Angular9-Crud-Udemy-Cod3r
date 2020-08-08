6470a6f90dada4a28c79e69a63b5ee4597239832
Isac Amaral <isac.redua@gmail.com>
2020-08-08 13:43:18 -0300

    implementação do CRUD

---
+++ frontend/src/app/components/product/product.service.ts
@@ -0,0 +1,46 @@
+import { Product } from './product.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseURL = 'http://localhost:3001/products'
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    })
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseURL, product)
+  }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseURL)
+  }
+
+  readById(id: number): Observable<Product> {
+    const url: string = `${this.baseURL}/${id}`
+    return this.http.get<Product>(url)
+  }
+  
+  update(product: Product): Observable<Product> {
+    const url: string = `${this.baseURL}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
+  
+  delete(id: number): Observable<Product> {
+    const url: string = `${this.baseURL}/${id}`
+    return this.http.delete<Product>(url)
+  }
+}
